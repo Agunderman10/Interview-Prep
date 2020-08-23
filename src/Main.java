8c75b93234b0f81508b8c4606770c860284533e6
agunderman10 <agunderman10@gmail.com>
2020-08-23 15:52:30 -0400

    Made code more modular and dealt with is negative edge case

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		ReverseInteger reverseInteger = new ReverseInteger();
-		int num = 3245;
+		int num = -3245;
 		System.out.println(num);
 		System.out.println(reverseInteger.reverseBruteForce(num));
 	}
--- src/ReverseInteger.java
+++ src/ReverseInteger.java
@@ -3,20 +3,49 @@
  * Brute force solution
  */
 public class ReverseInteger {
-	public int reverseBruteForce(int x) { // x = 3245
-		char[] chars = ("" + x).toCharArray();
+	
+	private boolean isNegative(int num) {
+		if(num < 0) {
+			return true;
+		}
+		return false;
+	}
+	
+	private int makePositive(int num) {
+		if(isNegative(num)) {
+			return num * - 1;
+		}
+		return num;
+	}
+	
+	private int loopThroughCharArrayAndReverse(char[] chars) {
 		int length = chars.length;
 		char temp;
 		char front;
 		char back;
+		
 		for(int i = 0; i < length / 2; i++) 
 		{
-			back = chars[i]; // front is now 3
+			back = chars[i];
 			front = chars[length - 1 - i];
 			temp = front;
 			chars[i] = temp;
 			chars[length - 1 - i] = back;
 		}
 		return Integer.parseInt(new String(chars));
+	}
+		
+	
+	public int reverseBruteForce(int x) {
+		char[] chars;
+
+		if(!isNegative(x)) {
+			chars = ("" + x).toCharArray();
+			return loopThroughCharArrayAndReverse(chars);
+		}
+		else {
+			chars = ("" + makePositive(x)).toCharArray();
+			return loopThroughCharArrayAndReverse(chars) * -1;
+		}
 	}
 }

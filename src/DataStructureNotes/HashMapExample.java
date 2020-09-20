6a16a7d59f44da6fcb083a0f760b130ed585aa20
agunderman10 <agunderman10@gmail.com>
2020-09-20 05:24:20 -0400

    Added hash map use example class

---
+++ src/DataStructureNotes/HashMapExample.java
@@ -0,0 +1,25 @@
+package DataStructureNotes;
+
+import java.util.HashMap;
+
+public class HashMapExample {
+
+	public static void main(String[] args) {
+		// (key, value) pair
+		HashMap<Integer, Integer> map = new HashMap<>();
+		
+		map.put(0, 23);
+		map.put(1, 24);
+		map.put(2, 89);
+		map.put(3, -34);
+		map.remove(3);
+		
+		
+		System.out.println(map);
+		System.out.println(map.get(2));
+		System.out.println(map.containsValue(24));
+		System.out.println(map.containsKey(3));
+
+	}
+
+}
--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,11 @@
+import java.util.Random;
+import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
-		ReverseInteger reverseInteger = new ReverseInteger();
-		int num = -324233245;
-		System.out.println(num);
-		System.out.println(reverseInteger.reverseBruteForce(num));
+	    
 	}
+
 
 }
